9b8c888912c7ad9cfdcbde77873a8440743880d0
Haakon Winther <haakonwinther@gmail.com>
2022-10-20 11:04:08 +0200

    Updated some text

--- src/About.js
+++ src/About.js
@@ -5,18 +5,18 @@ const unpaidSideHustlesWithBigDreams = [
   {
     name: 'Amexvenn.no',
     url: 'https://www.amexvenn.no',
-    desc: 'En nettside som gir en oversikt over hvor en kan bruke american express som betalingsmåte i Norge. Laget for å kunne opptjene mest mulig eurobonuspoeng via kredittkortet sitt.',
+    desc: 'En nettside som gir en oversikt over hvor en kan bruke american express som betalingsmåte i Norge. Laget for å kunne opptjene mest mulig eurobonuspoeng via kredittkortet sitt. Før var denne informasjonen delt i veldig lange tråder på diverse forum på nettet, men jeg samlet alt i en database og hoster det på nettet.  ',
   },
 
   {
     name: 'Parkettlegger.no',
     url: 'https://www.parkettlegger.no',
-    desc: 'En kalkulator som regner ut hvordan du skal legge parkettgulv slik at det er flytende. Laget i forbindelse med oppussing av leilighet.',
+    desc: 'En kalkulator som regner ut hvordan du skal legge parkettgulv slik at det er flytende. Laget i forbindelse med oppussing av leilighet. Synes det var tungvindt å regne ut for hånd hver gang.',
   },
   {
     name: 'Petimeter.io',
     url: 'https://petimeter.io',
-    desc: 'En webapp for regsitrering av avvik og rapportering i servicebransjen. Laget med firebase og cloud functions.',
+    desc: 'En webapp for regsitrering av avvik og rapportering i servicebransjen. Laget i sammerbeid med min gamle sjef. Prøver å erstatte et stykke papir. Laget med firebase og cloud functions.',
   },
 ];
 export const About = () => {
@@ -40,7 +40,7 @@ export const About = () => {
                 {' '}
                 Oslo Market Solutions
               </a>
-              , noe jeg har gjort siden Januar 2017
+              , noe jeg har gjort siden Januar 2017.
             </span>
             <a
               rel="noopener noreferrer"
@@ -50,8 +50,16 @@ export const About = () => {
               {' '}
               mnemonic
             </a>
-            , noe jeg har gjort siden Desember 2020. Utvikling i React og
-            TypeScript.
+            , noe jeg har gjort siden Desember 2020. Jeg jobber inhouse i
+            avdelingen MSS hvor jeg jobber med utviklingen av{' '}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.mnemonic.io/solutions/managed-detection-and-response/"
+            >
+              Argus.
+            </a>{' '}
+            All utvikling foregår i React og TypeScript.
           </p>
         </div>
         <div>
